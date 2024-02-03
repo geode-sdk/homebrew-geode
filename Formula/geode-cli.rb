faca1c155b168077d4c4f73b68f6c3fdadeb6e18
GeodeBot <hjfodgames@gmail.com>
2024-02-03 10:25:56 +0000

    Update CLI

--- Formula/geode-cli.rb
+++ Formula/geode-cli.rb
@@ -1,8 +1,8 @@
 class GeodeCli < Formula
   desc "CLI tool for Geode"
   homepage "https://geode-sdk.org"
-  url "https://github.com/geode-sdk/cli/releases/download/v2.8.0/geode-cli-v2.8.0-mac.zip"
-  sha256 "3acd1112264814fd9f48902eba46e74ee1abfe62609e773120695aec844b24e3"
+  url "https://github.com/geode-sdk/cli/releases/download/v2.9.0/geode-cli-v2.9.0-mac.zip"
+  sha256 "73bf3c40be1742f4ddfb8ad7cb94144b26171d3b99b7cd7617367c07f33912d4"
   license "BSL-1.0"
 
   def install
